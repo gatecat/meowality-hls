18fc74ffff63a20a6a184e3b5e6b6b911bb5452d
gatecat <gatecat@ds0.me>
2022-04-27 19:59:58 +0100

    Rework lowering API again
    Signed-off-by: gatecat <gatecat@ds0.me>

---
+++ src/backend/low_netlist.rs
@@ -0,0 +1,36 @@
+use rustc_hash::FxHashMap;
+use crate::core::{IdString, BitVector, BasicOp, OperandType, Constant};
+use crate::design::PortDir;
+
+/*
+Our final IL, as a shim between a Design and a backend
+
+This lowers away many of the complex primitives used in a Design; as well as things like un-abstracting clocks
+and ready/valid handshaking.
+
+The end result is something that is more-or-less directly serialisable to RTLIL
+*/
+pub enum LowPrim {
+	Null,
+	Input,
+	Op {ty: BasicOp, a: IdString, b: IdString},
+	Const {val: BitVector},
+	Assign {src: IdString},
+	BitSelect {src: IdString, start: usize, end: usize},
+	Cat {src: Vec<IdString>},
+	Mux {src: Vec<IdString>, sel: IdString},
+	
+}
+
+pub struct LowNode {
+	pub name: IdString,
+	pub dir: Option<PortDir>,
+	pub value: LowPrim,
+	pub attrs: FxHashMap<IdString, Constant>,
+	pub typ: OperandType,
+}
+
+pub struct LowNetlist {
+	pub name: IdString,
+	pub node: FxHashMap<IdString, LowNode>,
+}
--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,15 +1,1 @@
-use crate::core::{IdString, BitVector, BasicOp, OperandType, Constant};
-use crate::design::PortDir;
-use rustc_hash::FxHashMap;
-
-pub trait Backend {
-	fn init(&mut self, mod_name: IdString);
-	fn add_node(&mut self, name: IdString, typ: OperandType, attrs: &FxHashMap<IdString, Constant>);
-	fn add_port(&mut self, name: IdString, typ: OperandType, dir: PortDir, attrs: &FxHashMap<IdString, Constant>);
-	fn add_const(&mut self, val: BitVector) -> IdString;
-	fn add_basicop(&mut self, name: IdString, typ: BasicOp, inputs: &[IdString], output: IdString, attrs: &FxHashMap<IdString, Constant>);
-	fn add_mux(&mut self, name: IdString, a: IdString, b: IdString, s: IdString, q: IdString, attrs: &FxHashMap<IdString, Constant>);
-	fn add_dff(&mut self, name: IdString, clk: IdString, rst: IdString, en: IdString, d: IdString, q: IdString, attrs: &FxHashMap<IdString, Constant>);
-	fn add_assign(&mut self, src: IdString, dst: IdString);
-	fn finalise(&mut self);
-}
+pub mod low_netlist;
